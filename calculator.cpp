cd195b6fc801c1aa275298bf57cde19e37289e22
Nikita Huynh <nikitahuynh@csilvm-20.cs.ucsb.edu>
2024-12-04 10:40:42 -0800

    Fixed divide by 0 errors.

--- calculator.cpp
+++ calculator.cpp
@@ -30,6 +30,10 @@ int main() {
             cout << "Result: " << result << endl;
             break;
         case '/':
+            if (num2==0) {
+                cerr << "Divison by error\n";
+                exit(1);
+            }
             result = num1 / num2;  // No check for dividing by 0
             cout << "Result: " << result << endl;
             break;
